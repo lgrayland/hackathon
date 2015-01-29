2ad2e1fe059e162d420ac4b9326e01aa377838d8
salamalala <hello@ninaregli.ch>
2015-01-29 17:56:35 +0000

    working on angular functions

--- js/main.js
+++ js/main.js
@@ -26,90 +26,114 @@
   var app = angular.module('hackathonApp', ['ngAnimate']);
 
   app.controller('HackathonController', function($scope, $http){
-    
+
     $scope.students = [];
+    $scope.matchedStudents = [];
 
     $http.get("data/students.json").success(function(data){
       $scope.students = data;
       console.log(data);
+
+      $scope.selectCard = function(student) {
+        if ($scope.first) {
+          $scope.second = student;
+          console.log(student);
+
+
+        } else {
+          $scope.first = student;
+        }
+
+      };
+
+      $scope.isSelected = function(student) {
+        return $scope.first == student || $scope.second == student
+      }
+
+      $scope.isMatched = function(student) {
+        return false; // is the student in the array of matched students?
+      }
+
+
     });
 
-   $scope.selectStudent = function(student) {
-       $scope.selectedStudent = student;
-   };
-    
-  //   $scope.students = students.sort(function() {
-  //     return .5 - Math.random();
-  //   });
-
-
-
-    
 
 
   });
+
+
 })();
 
 
-  var myGame = myGame || {};
-  var firstCard;
-  var firstChoice;
-  var secondCard;
-  var secondChoice;
-  var clicked = 0;
-  var moves = 0;
 
-  myGame.initialize = function(){
+  //      moves ++;
+  //      clicked ++;
 
-    $(".cards").on("click", function(){
-      moves ++;
-      clicked ++;
+  //      if (clicked === 1){
+  //        firstCard = $(this);  
+  //        if (!firstCard.hasClass("selected") || !firstCard.hasClass("matched")){
+  //          firstChoice = firstCard.addClass("selected").attr('class').split(' ')[0];      
+  //        }
+  //        console.log(firstCard)
+  //      } else if (clicked === 2){
+  //        secondCard = $(this);
+  //        if (!secondCard.hasClass("selected") || !secondCard.hasClass("matched")){
+  //         secondChoice = secondCard.addClass("selected").attr('class').split(' ')[0];
 
-      if (clicked === 1){
-        firstCard = $(this);  
-        if (!firstCard.hasClass("selected") || !firstCard.hasClass("matched")){
-          firstChoice = firstCard.addClass("selected").attr('class').split(' ')[0];      
-        }
-        console.log(firstCard)
-      } else if (clicked === 2){
-        secondCard = $(this);
-        if (!secondCard.hasClass("selected") || !secondCard.hasClass("matched")){
-         secondChoice = secondCard.addClass("selected").attr('class').split(' ')[0];
+  //       console.log(secondCard);
+  //       if (firstChoice === secondChoice){
+  //        firstCard.addClass("matched");
+  //        secondCard.addClass("matched");
+  //      } else {
+  //        setTimeout(function() {
+  //          firstCard.removeClass("selected");
+  //          secondCard.removeClass("selected");
+  //        }, 1000);      
+  //      }
 
-       console.log(secondCard);
-       if (firstChoice === secondChoice){
-        firstCard.addClass("matched");
-        secondCard.addClass("matched");
-      } else {
-        setTimeout(function() {
-          firstCard.removeClass("selected");
-          secondCard.removeClass("selected");
-        }, 1000);      
-      }
+  //      clicked = 0;
 
-      clicked = 0;
+  //      }
 
-      }
 
-    // if cards with class matched = 24 
-    if ($(".matched").length >= 34){
-      $("#number-of-moves").text(moves); 
-      $("#gameOver").show();
+  //  };
 
-    }
-  }
+  // //   $scope.students = students.sort(function() {
+  // //     return .5 - Math.random();
+  // //   });
 
 
 
 
-});
+  // var myGame = myGame || {};
+  // var firstCard;
+  // var firstChoice;
+  // var secondCard;
+  // var secondChoice;
+  // var clicked = 0;
+  // var moves = 0;
+
+  
+
+//     // if cards with class matched = 24 
+//     if ($(".matched").length >= 34){
+//       $("#number-of-moves").text(moves); 
+//       $("#gameOver").show();
+
+//     }
+//   }
 
 
 
-  };
+
+// });
 
 
-  $(function(){
-    myGame.initialize();
 
-  });
+//   };
+
+
+//   $(function(){
+//     myGame.initialize();
+
+//   });
