bf8b6dc02aaeedf93a91112b49eae116548b5c89
Luke Grayland <lgrayland@gmail.com>
2015-01-30 10:18:11 +0000

    removed some unused code

--- js/main.js
+++ js/main.js
@@ -50,101 +50,20 @@
       };
 
       $scope.isSelected = function(student) {
-        return $scope.first == student || $scope.second == student
-      }
+        return $scope.first == student || $scope.second == student;
+      };
 
       $scope.isMatched = function(student) {
         // return $scope.first.name == $scope.second.name;
         // return false; // is the student in the array of matched students?
         return matchedStudents.indexOf(student) != -1;
-      }
+      };
 
       function checkForMatch(){
         if ($scope.first.name == $scope.second.name){
           matchedStudents.push($scope.first, $scope.second);
-        };
+        }
       }
-
-
     });
-
-
-
-});
-
-
+  });
 })();
-
-
-
-  //      moves ++;
-  //      clicked ++;
-
-  //      if (clicked === 1){
-  //        firstCard = $(this);  
-  //        if (!firstCard.hasClass("selected") || !firstCard.hasClass("matched")){
-  //          firstChoice = firstCard.addClass("selected").attr('class').split(' ')[0];      
-  //        }
-  //        console.log(firstCard)
-  //      } else if (clicked === 2){
-  //        secondCard = $(this);
-  //        if (!secondCard.hasClass("selected") || !secondCard.hasClass("matched")){
-  //         secondChoice = secondCard.addClass("selected").attr('class').split(' ')[0];
-
-  //       console.log(secondCard);
-  //       if (firstChoice === secondChoice){
-  //        firstCard.addClass("matched");
-  //        secondCard.addClass("matched");
-  //      } else {
-  //        setTimeout(function() {
-  //          firstCard.removeClass("selected");
-  //          secondCard.removeClass("selected");
-  //        }, 1000);      
-  //      }
-
-  //      clicked = 0;
-
-  //      }
-
-
-  //  };
-
-  // //   $scope.students = students.sort(function() {
-  // //     return .5 - Math.random();
-  // //   });
-
-
-
-
-  // var myGame = myGame || {};
-  // var firstCard;
-  // var firstChoice;
-  // var secondCard;
-  // var secondChoice;
-  // var clicked = 0;
-  // var moves = 0;
-
-  
-
-//     // if cards with class matched = 24 
-//     if ($(".matched").length >= 34){
-//       $("#number-of-moves").text(moves); 
-//       $("#gameOver").show();
-
-//     }
-//   }
-
-
-
-
-// });
-
-
-
-//   };
-
-
-//   $(function(){
-//     myGame.initialize();
-
-//   });
